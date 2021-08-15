4f3c8ae4366c25100ecb285d07235a8d8b88a94b
cat-bear-get <2416078929@qq.com>
2021-08-15 11:39:39 +0800

    引用常量

---
+++ test6.cpp
@@ -0,0 +1,20 @@
+//
+// Created by jingwei on 2021/8/15.
+//
+#include <iostream>
+
+using namespace std;
+
+void showValue(const int &value) {
+    //value=100;
+    cout << "value=" << value << endl;
+}
+
+int main() {
+    //常量引用
+    //使用场景：用来修饰形参，防止误操作
+
+    //const int & ref = 10;
+    int a = 100;
+    showValue(a);
+}
